5b9028ee5d51219fdab97b2ef29f8dcfd0774ed1
Thomas66 <thomassalvaterra2001@gmail.com>
2023-10-26 11:55:48 +0200

    ifnal version 2.0

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const nomeUtente = prompt( "Inserisci il tuo nome" );
+console.log(nomeUtente);
+
 const cognomeUtente = prompt( "Inserisci il tuo cognome" );
+console.log(cognomeUtente);
+
 const coloreUtente = prompt ( "Inserisci il tuo colore preferito" );
+console.log(coloreUtente);
 
 document.getElementById("password").innerHTML = "La tua password è " + nomeUtente + cognomeUtente + coloreUtente + "#23"
+
